9b2d367dc93e769fddf36380649edb4eb21ad3ec
Abigail Sarzuri <abigailsarzuri@gmail.com>
2023-07-19 09:43:43 -0300

    add style in tags page

--- src/components/Latest/Latest.jsx
+++ src/components/Latest/Latest.jsx
@@ -6,8 +6,8 @@ const Latest = ({ name, collection }) => {
 
   return (
     <div className="relative flex flex-col w-full gap-10 px-5 2xl:self-center 2xl:max-w-screen-xl 2xl:p-0 lg:px-20">
-      <h2 className="capitalize flex lg:text-lg 2xl:text-xl  font-medium lg:pl-5 relative before:content-[''] before:absolute before:w-[1.25rem] lg:before:w-2 before:h-[1.75rem] before:left-[-1.7rem] lg:before:left-0 before:bg-orange-500 after:content-[''] after:w-full after:h-[1.75rem] after:bg-orange-500 after:right-0 after:flex-[1 0] after:relative after:ml-[-0.5rem] lg:after:ml-5 after:top-0 after:left-5 lg:after:left-0">
-        {name}
+      <h2 className="capitalize flex lg:text-lg 2xl:text-xl font-medium lg:pl-5 relative before:content-[''] before:absolute before:w-[1.25rem] lg:before:w-2 before:h-[1.75rem] before:left-[-1.7rem] lg:before:left-0 before:bg-orange-500 after:content-[''] after:w-full after:h-[1.75rem] after:bg-orange-500 after:right-0 after:flex-[1 0] after:relative after:ml-[-0.5rem] lg:after:ml-0 after:top-0 after:left-5 lg:after:left-0">
+        <span className="w-full lg:w-64">{name}</span>
       </h2>
       <div className="flex flex-col w-full gap-10">
         {collection?.map((item, index) => (
--- src/pages/Tag/Tag.jsx
+++ src/pages/Tag/Tag.jsx
@@ -1,15 +1,49 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { Latest } from '@components';
+import { AiFillTags } from 'react-icons/ai';
+
+import { Latest, Paginate } from '@components';
+import { getTotalPages } from '@utils';
 
 const Tag = () => {
   const { tag } = useParams();
   const { tagDetail } = useSelector((state) => state.newsState);
   const tagModified = tag.replace(/-/g, ' ');
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+
+  const totalItems = tagDetail.length;
+  const totalPages = getTotalPages(totalItems, pageSize);
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = currentPage * pageSize;
+
+  const currentTagsPage = tagDetail.slice(startIndex, endIndex);
+
+  const changePage = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div>
-      <Latest name={tagModified} collection={tagDetail} />
+    <div className="flex flex-col gap-20 pb-10 xl:w-full lg:pb-20 lg:min-h-screen">
+      <div className="bg-[#101010] w-full px-5 flex flex-col items-center justify-center py-10 h-36">
+        <span className="flex items-center gap-1 text-zinc-400">
+          <AiFillTags className="text-xl transition-all lg:text-2xl" />
+          TAGS
+        </span>
+        <h3 className="text-2xl font-medium uppercase">
+          &quot;{tagModified}&quot;
+        </h3>
+      </div>
+      <Latest name={tagModified} collection={currentTagsPage} />
+      <Paginate
+        length={tagDetail.length}
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onChangePage={changePage}
+      />
     </div>
   );
 };
